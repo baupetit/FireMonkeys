ffa48dfb9ce620149ddfb18555f77cd53841447a
Benjamin Aupetit <benjamin.aupetit@ensimag.imag.fr>
2010-05-27 14:27:30 +0200

    solver solver

--- Src/solver.cpp
+++ Src/solver.cpp
@@ -1,9 +1,14 @@
 #define SIZE ((N+2)*(N+2)*(N+2))
-#define SWAP(x0,x) {float * tmp=x0;x0=x;x=tmp;}
-#define FOR_EACH_CELL for ( k=1 ; k< N+1 ; k++ ) { for ( j=1 ; j<N+1 ; j++ ) { for( i=1; i<N+1 ; i++ ) {
-#define END_FOR }}}
 
 #include "solver.h"
+
+#include <cmath>
+
+inline void SWAP( float* &a , float* &b){
+	float *tmp = a ;
+	a = b ;
+	b = tmp ;
+}
 
 Solver::Solver( int N ) : _N(N) {
 	
@@ -16,6 +21,11 @@ Solver::Solver( int N ) : _N(N) {
 	_v0 = new float[SIZE];		
 	_w = new float[SIZE];
 	_w0 = new float[SIZE];		
+	_srcd = new float[SIZE];
+	_srcu = new float[SIZE];
+	_srcv = new float[SIZE];
+	_srcw = new float[SIZE];
+	
 
 	int i;
 
@@ -28,6 +38,7 @@ Solver::Solver( int N ) : _N(N) {
 		_v0[i] = 0.0f;
 		_w[i] = 0.0f;
 		_w0[i] = 0.0f;
+		_srcd[i] = 0.0f;
 	}
 }
 
@@ -63,14 +74,14 @@ int Solver::getSize() const{
 
 void Solver::setDensity( int i , int j , int k , float dens ){
 	int N = _N ;
-	_d[IX(i,j,k)] = dens ;
+	_srcd[IX(i,j,k)] = dens ;
 }
 
 void Solver::setVelocity( int i, int j , int k , float u, float v, float w ){
 	int N = _N ;
-	_u[IX(i,j,k)] = u ;
-	_v[IX(i,j,k)] = v ;
-	_w[IX(i,j,k)] = w ;
+	_srcu[IX(i,j,k)] = u ;
+	_srcv[IX(i,j,k)] = v ;
+	_srcw[IX(i,j,k)] = w ;
 }
 
 void addSource ( int N, float *x , float *s , float dt )
@@ -81,9 +92,8 @@ void addSource ( int N, float *x , float *s , float dt )
 
 void setBoundaries ( int N, int b, float *x )
 {
-/*
 	int i,j;
-
+/*
 	for ( i=1 ; i<=N ; i++ ) {
 		for( j=1 ; j<=N ; j++ ) {
 			x[IX(0  ,i,j)] = b==1 ? -x[IX(1,i,j)] : x[IX(1,i,j)];
@@ -104,17 +114,6 @@ void setBoundaries ( int N, int b, float *x )
 	x[IX(N+1,0  ,N+1)] = (x[IX(N,0  ,N+1)]+x[IX(N+1,1,N+1)]+x[IX(N+1,0  ,N)])/3;
 	x[IX(N+1,N+1,N+1)] = (x[IX(N,N+1,N+1)]+x[IX(N+1,N,N+1)]+x[IX(N+1,N+1,N)])/3;
 */
-	for ( int i=1 ; i<=N ; i++ ) {
-		for( int j=1 ; j<=N ; j++ ) {
-			x[IX(0  ,i,j)] = 0;
-			x[IX(N+1,i,j)] = 0;
-			x[IX(i,0  ,j)] = 0;
-			x[IX(i,N+1,j)] = 0;
-			x[IX(i,j  ,0)] = 0;
-			x[IX(i,j  ,N+1)] = 0;
-		}
-	}
-
 }
 
 void linearSolve ( int N, int b, float * x, float * x0, float a, float c )
@@ -122,14 +121,18 @@ void linearSolve ( int N, int b, float * x, float * x0, float a, float c )
 	int i, j, k, l;
 
 	for ( l=0 ; l<20 ; l++ ) {
-		FOR_EACH_CELL
-			x[IX(i,j,k)] = ( x0[IX(i,j,k)] + a*( x[IX(i-1,j,k)] +
-							     x[IX(i+1,j,k)] +
-							     x[IX(i,j-1,k)] +
-							     x[IX(i,j+1,k)] +
-							     x[IX(i,j,k-1)] +
-							     x[IX(i,j,k+1)] ))/c;
-		END_FOR
+		for( k=1; k<N+1; ++k ){ 
+			for( j = 1; j<N+1 ; ++j) { 
+				for( i=1 ; i<N+1 ; ++i ){
+					x[IX(i,j,k)] = ( x0[IX(i,j,k)] + a*( x[IX(i-1,j,k)] +
+									     x[IX(i+1,j,k)] +
+									     x[IX(i,j-1,k)] +
+									     x[IX(i,j+1,k)] +
+									     x[IX(i,j,k-1)] +
+									     x[IX(i,j,k+1)] ))/c;
+				}
+			}
+		}
 		setBoundaries ( N, b, x );
 	}
 }
@@ -146,37 +149,41 @@ void advect ( int N, int b, float * d, float * d0, float * u, float * v, float *
 	float x, y, z, s0, t0, r0, s1, t1, r1, dt0;
 
 	dt0 = dt*N;
-	FOR_EACH_CELL
-		x = i-dt0*u[IX(i,j,k)]; 
-	        y = j-dt0*v[IX(i,j,k)];
-		z = k-dt0*w[IX(i,j,k)];
+	for( k=1; k<N+1; ++k ){ 
+		for( j = 1; j<N+1 ; ++j) { 
+			for( i=1 ; i<N+1 ; ++i ){
+				x = i-dt0*u[IX(i,j,k)]; 
+				y = j-dt0*v[IX(i,j,k)];
+				z = k-dt0*w[IX(i,j,k)];
 
-		if (x<0.5f) x=0.5f; 
-		if (x>N+0.5f) x=N+0.5f; 
-		i0=(int)x; 
-		i1=i0+1;
-
-	        if (y<0.5f) y=0.5f; 
-		if (y>N+0.5f) y=N+0.5f; 
-		j0=(int)y; 
-		j1=j0+1;
-
-		if (z<0.5f) z=0.5f; 
-		if (z>N+0.5f) z=N+0.5f; 
-		k0=(int)z; 
-		k1=k0+1;
-
-
-	        s1 = x-i0; s0 = 1-s1; 
-		t1 = y-j0; t0 = 1-t1; 
-		r1 = z-k0; r0 = 1-r1;
-
-	        d[IX(i,j,k)] = s0*(t0*(r0*d0[IX(i0,j0,k0)] + r1*d0[IX(i0,j0,k1)]) + 
-				 t1*(r0*d0[IX(i0,j1,k0)] + r1*d0[IX(i0,j1,k1)]))
-			   +
-			       s1*(t0*(r0*d0[IX(i1,j0,k0)] + r1*d0[IX(i1,j0,k1)]) + 
-				 t1*(r0*d0[IX(i1,j1,k0)] + r1*d0[IX(i1,j1,k1)]));
-	END_FOR
+				if (x<0.5f) x=0.5f; 
+				if (x>N+0.5f) x=N+0.5f; 
+				i0=(int)x; 
+				i1=i0+1;
+				
+				if (y<0.5f) y=0.5f; 
+				if (y>N+0.5f) y=N+0.5f; 
+				j0=(int)y; 
+				j1=j0+1;
+				
+				if (z<0.5f) z=0.5f; 
+				if (z>N+0.5f) z=N+0.5f; 
+				k0=(int)z; 
+				k1=k0+1;
+				
+				
+				s1 = (x-i0); s0 = 1-s1; 
+				t1 = (y-j0); t0 = 1-t1; 
+				r1 = (z-k0); r0 = 1-r1;
+				
+				d[IX(i,j,k)] = (s0*(t0*(r0*d0[IX(i0,j0,k0)] + r1*d0[IX(i0,j0,k1)]) + 
+						    t1*(r0*d0[IX(i0,j1,k0)] + r1*d0[IX(i0,j1,k1)]))
+						+
+						s1*(t0*(r0*d0[IX(i1,j0,k0)] + r1*d0[IX(i1,j0,k1)]) + 
+						    t1*(r0*d0[IX(i1,j1,k0)] + r1*d0[IX(i1,j1,k1)])));
+			}
+		}
+	}
 	setBoundaries ( N, b, d );
 }
 
@@ -184,21 +191,29 @@ void project ( int N, float * u, float * v, float *w, float * p, float * div )
 {
 	int i, j, k;
 
-	FOR_EACH_CELL
-		div[IX(i,j,k)] = -(u[IX(i+1,j,k)]-u[IX(i-1,j,k)]+
-				   v[IX(i,j+1,k)]-v[IX(i,j-1,k)]+
-				   w[IX(i,j,k+1)]-w[IX(i,j,k-1)] )/(3*N);
-	        p[IX(i,j,k)] = 0;
-	END_FOR	
+	for( k=1; k<N+1; ++k ){ 
+		for( j = 1; j<N+1 ; ++j) { 
+			for( i=1 ; i<N+1 ; ++i ){
+				div[IX(i,j,k)] = -(u[IX(i+1,j,k)]-u[IX(i-1,j,k)]+
+						   v[IX(i,j+1,k)]-v[IX(i,j-1,k)]+
+						   w[IX(i,j,k+1)]-w[IX(i,j,k-1)] )/(3*N);
+				p[IX(i,j,k)] = 0;
+			}
+		}
+	}	
 	setBoundaries ( N, 0, div ); setBoundaries ( N, 0, p );
 
 	linearSolve ( N, 0, p, div, 1, 6 );
 
-	FOR_EACH_CELL
-		u[IX(i,j,k)] -= 0.5f*N*(p[IX(i+1,j,k)]-p[IX(i-1,j,k)]);
-	        v[IX(i,j,k)] -= 0.5f*N*(p[IX(i,j+1,k)]-p[IX(i,j-1,k)]);
-		w[IX(i,j,k)] -= 0.5f*N*(p[IX(i,j,k+1)]-p[IX(i,j,k-1)]);
-	END_FOR
+	 for( k=1; k<N+1; ++k ){ 
+		 for( j = 1; j<N+1 ; ++j) { 
+			 for( i=1 ; i<N+1 ; ++i ){
+				 u[IX(i,j,k)] -= N*(p[IX(i+1,j,k)]-p[IX(i-1,j,k)])/3;
+				 v[IX(i,j,k)] -= N*(p[IX(i,j+1,k)]-p[IX(i,j-1,k)])/3;
+				 w[IX(i,j,k)] -= N*(p[IX(i,j,k+1)]-p[IX(i,j,k-1)])/3;
+			 }
+		 }
+	 }
 	setBoundaries ( N, 1, u ); 
 	setBoundaries ( N, 2, v ); 
 	setBoundaries( N, 3, w);
@@ -206,16 +221,16 @@ void project ( int N, float * u, float * v, float *w, float * p, float * div )
 
 void Solver::densitiesStep ( float diff, float dt )
 {	
-	addSource ( _N, _d, _d0, dt );
+	addSource ( _N, _d, _srcd, dt );
 	SWAP ( _d0, _d ); diffuse ( _N, 0, _d, _d0, diff, dt );
 	SWAP ( _d0, _d ); advect ( _N, 0, _d, _d0, _u, _v, _w, dt );
 }
 
 void Solver::velocitiesStep ( float visc, float dt )
 {
-	addSource ( _N, _u, _u0, dt ); 
-	addSource ( _N, _v, _v0, dt );
-	addSource ( _N, _w, _w0, dt );
+	addSource ( _N, _u, _srcu, dt ); 
+	addSource ( _N, _v, _srcv, dt );
+	addSource ( _N, _w, _srcw, dt );
 	SWAP ( _u0, _u ); diffuse ( _N, 1, _u, _u0, visc, dt );
 	SWAP ( _v0, _v ); diffuse ( _N, 2, _v, _v0, visc, dt );
 	SWAP ( _w0, _w ); diffuse ( _N, 3, _w, _w0, visc, dt );
